abed4a1b7c125ae446277d66cea39605a6c6aff9
sato wataru <watarusato105@gmail.com>
2024-03-19 11:55:09 +0900

    #7 クライアントのベース作成

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+import { onMounted, Ref } from '@vue/runtime-core';
+import { ref} from 'vue';
+import { QForm} from "quasar";
+
+const locationForm:Ref<QForm | undefined> = ref();
+
+type LocationFormValue = {
+  lat: number | null,
+  lon: number | null
+}
+const locationFormValue:Ref<LocationFormValue> = ref({
+  lat: null,
+  lon: null
+});
+
+const labels = {
+  lat: "緯度",
+  lon: "経度"
+};
+
+const validationRules = {
+  lat: [],
+  lon: []
+};
+
+onMounted(() => {
+  console.log("start sakurasaku")
+});
+</script>
+
+<template>
+  <!-- TODO:header -->
+  <q-form ref="locationForm">
+    <div class="row">
+      <div class="col-6 q-pa-xs">
+        <q-input
+          v-model="locationFormValue.lat"
+          type="number"
+          :label="labels.lat"
+          :rules="validationRules.lat"
+          clearable
+        ></q-input>
+      </div>
+      <div class="col-6 q-pa-xs">
+        <q-input
+          v-model="locationFormValue.lon"
+          type="number"
+          :label="labels.lon"
+          :rules="validationRules.lon"
+          clearable
+        ></q-input>
+      </div>
+    </div>
+  </q-form>
+  sakurasaku
+</template>
+
+<style scoped>
+</style>
